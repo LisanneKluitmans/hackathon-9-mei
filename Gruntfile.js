7d1695aeaaba67924497355eef6b6e07aa1c8bdd
Lisanne Kluitmans <lisanne@yoast.com>
2019-05-09 11:17:44 +0200

    Yay step 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,57 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-  });
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
+		copy: {
+			main: {
+				files: [{
+					expand: true,
+					cwd: './', // 'Current Working Directory'
+					src: 'index.html', // Read everything inside the cwd
+					dest: 'toZip/' // Destination folder
+				},
+					{
+						expand: true,
+						cwd: 'source-files/', // 'Current Working Directory'
+						src: ['**', '!**.scss'], // Read everything inside the cwd
+						dest: 'toZip/assets' // Destination folder
+					}]
+			}
+		},
+		sass: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'source-files/',
+					src: ['*.scss'],
+					dest: 'toZip/assets',
+					ext: '.css'
+				}]
+			}
+		},
+		compress: {
+			main: {
+				options: {
+					archive: 'lisanne.zip'
+				},
+				expand: true,
+				cwd: 'toZip',
+				src: ['**'],
+				dest: './'
+			}
+		}
+	});
 
-  // Load tasks here.
+	// Load tasks here.
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-compress');
 
-  // Define aliases here.
-  grunt.registerTask('default', 'My default task description', function() {
-    grunt.log.writeln( 'This is the default grunt task, create a new task and configure.' );
-  });
+
+	// Define aliases here.
+	grunt.registerTask('default', [ 'copy', 'sass', 'compress' ] );
 
 };
